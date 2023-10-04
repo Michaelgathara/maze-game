8281d0c466c4d88c9025c99764a303384ed85897
hamidc10 <98240536+hamidc10@users.noreply.github.com>
2023-10-03 20:28:31 -0500

    Added moves and euclidean dst (still need to test if it works)

--- a_starbot.py
+++ a_starbot.py
@@ -2,10 +2,11 @@ import sys
 import select
 import time
 import queue
+import math
 
 
 class bot:
-    def init(self, start, current_pos, cost, walls):
+    def __init__ (self, start, current_pos, cost, walls):
         self.start = start
         self.current_pos = current_pos
         # self.seen = seen # I don't think we quite need this, should be dynamic below. The bot should be init with nothing being seen (except where it's at ofc)
@@ -25,15 +26,55 @@ class bot:
         """
             I think this can be our actual move function, which should update current bot position
         """
-        pass
+         # It's also important we find out what the goal is, is it static across all mazes or can it change and be random based on the board?
+        while True:  
+            x,y=current
+            up=(x,y-1)
+            down=(x,y+1)
+            left=(x-1,y)
+            right=(x+1,y)
+            moves=[]
+            while select.select([sys.stdin,],[],[],0.0)[0]:
+                # read and process the next 1-line observation
+                obs = sys.stdin.readline()
+                obs = obs.split(" ")
+                if obs == []:
+                    pass
+                elif obs[0] == "bot":
+                    # update our own position
+                    x = int(float(obs[1]))+0.5
+                    y = int(float(obs[2]))+0.5
+                    self.current_pos=(x,y)
+                elif obs[0] == "wall":
+                    # ensure every wall we see is tracked in our walls set
+                    x0 = int(float(obs[1]))
+                    y0 = int(float(obs[2]))
+                    x1 = int(float(obs[3]))
+                    y1 = int(float(obs[4]))
+                    self.walls |= {(x0,y0,x1,y1)}
+    
+            if up not in self.walls:
+                moves.append(up)
+            if down not in self.walls:
+                moves.append(down)
+            if left not in self.walls:
+                moves.append(left)
+            if right not in self.walls:
+                moves.append(right)
+            
+            return moves
 
     def heuristic(self, goal):
         """
             We need heuristics to influence the search to make it better.
         """
-
-        # It's also important we find out what the goal is, is it static across all mazes or can it change and be random based on the board?
-        pass
+        #Used Euclidean  distance (similar to manhattan but allow for diagonals)
+        curr_x,curr_y=self.current_pos
+        goal_x,goal_y=goal
+        e_dst=math.sqrt((curr_x-goal_x)**2+(curr_y-goal_y)**2)
+        return e_dst
+        
+      
 
     def solver(self, goal):
         time.sleep(0.25)
@@ -53,14 +94,7 @@ class bot:
                 if next_position not in self.walls and next_position not in frontier.queue:
                     frontier.put((new_cost, next_position))
 
-        # while True:
-        #     while select.select([sys.stdin,],[],[],0.0)[0]:
-        #         obs = sys.stdin.readline()
-        #         obs = obs.split(" ")
-        #         if obs == []: pass
-        #         elif obs[0] == "bot":
-        #         self.current_pos=(float(obs[1]),float(obs[2]))
-        #         pass
+
 
 
 start_bot_pos = (0.5, 0.5)
@@ -70,5 +104,4 @@ walls_pos = set()
 
 print("himynameis A* bot", flush=True)
 a_star_bot = bot(start_bot_pos, current_bot_pos, cost, walls_pos)
-# Should I call walls ?
-# a_star_bot.walls()
+a_star_bot.walls()
