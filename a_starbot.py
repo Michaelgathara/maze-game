5ffcd2d109f34dfbcd3ebc506eba7c782d2b2930
michaelgathara <mikegtrm@gmail.com>
2023-10-20 03:42:32 -0500

    lord have mercy it works

--- a_starbot.py
+++ a_starbot.py
@@ -4,6 +4,9 @@ import select
 import time
 from queue import PriorityQueue
 
+terminal = sys.stdout
+logger = open("logs/output.log", "w")
+
 class Bot:
 
     def __init__(self):
@@ -17,12 +20,14 @@ class Bot:
         self.seen = set()
         self.dead = set()
         self.path = []
+        self.planned_path = []
+        self.backtrack_stack = []
         self.walls = set()
+        self.is_at_start = True
 
         for i in range(0, 11):
             self.walls |= {(i, 0, i + 1, 0), (i, 11, i + 1, 11), (0, i, 0, i + 1), (11, i, 11, i + 1)}
         print("himynameis A* bot", flush=True)
-        # Wait a few seconds for some initial sense data
         time.sleep(0.25)
 
     def successors(self):
@@ -44,13 +49,25 @@ class Bot:
             possible_actions.append((self.tx - 1, self.ty))  # move left
         
         if len(possible_actions) == 0:
-            self.dead |= {(self.tx, self.ty)}
-            self.path = self.path[:-1]
-            possible_actions.append(self.path[:-1])
+            if self.backtrack_stack:
+                backtrack_to = self.backtrack_stack.pop()
+                possible_actions.append(backtrack_to)
+            else:
+                self.dead |= {(self.tx, self.ty)}
+        else:
+            self.backtrack_stack.append((self.tx, self.ty))
 
         return possible_actions
-
     def astar(self):
+        if self.is_at_start and self.tx == self.home_x and self.ty == self.home_y:
+            self.is_at_start = False
+            self.goal_x = 10
+            self.goal_y = 10
+        elif not self.is_at_start and self.tx == self.goal_x and self.ty == self.goal_y:
+            self.is_at_start = True
+            self.goal_x = self.home_x
+            self.goal_y = self.home_y
+
         start = (self.tx, self.ty)
         goal = (self.goal_x, self.goal_y)
         frontier = PriorityQueue()
@@ -60,12 +77,16 @@ class Bot:
 
         while not frontier.empty():
             _, current = frontier.get()
-
-            if current == goal:
-                break
+            sys.stdout = logger
+            print(f"F: {frontier}")
+            sys.stdout = terminal
 
             for next_node in self.actions():
-                new_cost = cost_so_far[current] + 1  # Assuming a cost of 1 for each step
+                dx = abs(current[0] - next_node[0])
+                dy = abs(current[1] - next_node[1])
+                cost = 1.414 if dx == 1 and dy == 1 else 1  # 1.414 is sqrt(2), the diagonal distance
+                
+                new_cost = cost_so_far[current] + cost
                 try:
                     if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
                         cost_so_far[next_node] = new_cost
@@ -74,21 +95,23 @@ class Bot:
                         came_from[next_node] = current
                 except:
                     print(f"comment {cost_so_far}")
-
         path = []
         while current != start:
             path.append(current)
             current = came_from[current]
         path.append(start)
         path.reverse()
+        self.planned_path = path
         return path
 
     def heuristic(self, goal, current):
         return math.sqrt((goal[0] - current[0]) ** 2 + (goal[1] - current[1]) ** 2)
+    
 
 if __name__ == "__main__":
     bot = Bot()
-    
+    # bot.main()
+
     while True:
         while select.select([sys.stdin, ], [], [], 0.0)[0]:
             obs = sys.stdin.readline()
@@ -113,7 +136,7 @@ if __name__ == "__main__":
                 x1 = int(float(obs[3]))
                 y1 = int(float(obs[4]))
                 bot.walls |= {(x0, y0, x1, y1)}
-
+                
         if bot.path:
             if len(bot.path) > 0 and bot.path[-1] == (bot.tx, bot.ty):
                 bot.seen |= {(bot.tx, bot.ty)}
@@ -130,11 +153,7 @@ if __name__ == "__main__":
                     bot.seen = set()
 
                 bot.path = bot.astar()
-            # if (bot.tx, bot.ty) == (int(bot.tx), int(bot.ty)):
-            #     print("comment Bot has reached the desired tile at %s %s" % (bot.tx, bot.ty), flush=True)
             time.sleep(2)
             print("toward %s %s" % (bot.path[-1][0] + 0.5, bot.path[-1][1] + 0.5), flush=True)
-           
-        
         print("", flush=True)
         time.sleep(0.125)
